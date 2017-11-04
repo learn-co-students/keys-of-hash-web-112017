e73be5597ce36ae71b1e55a375429fc28fb22dc4
eal6gf <elum1234@gmail.com>
2017-11-04 00:58:34 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    a = []
+    arguments.each do |argument|
+      a << self.select{|k,v| argument == v}.keys
+    end
+    return a.flatten
   end
 end
