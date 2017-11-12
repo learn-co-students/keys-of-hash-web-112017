a8e09dd884a26dd551e494e2681fcd6928a63693
nspp99b <chromepanthers@gmail.com>
2017-11-12 13:17:19 -0500

    solved using splat, each, self

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arg_hash_values)
+    keys_array = []
+    arg_hash_values.each do |arg|
+      self.each do |key, value|
+        if value == arg
+          keys_array.push(key)
+        end
+      end
+    end
+    keys_array
   end
 end
