4867a50ee1a4959985218fa73483ee76ba8904c6
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-11-12 20:38:21 -0500

    completed keys of hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+      output = []
+      each {|k, v| output << k if arguments.include?(v)}
+      output
   end
 end
+
+
